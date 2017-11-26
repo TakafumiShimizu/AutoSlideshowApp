aac2fb374e6ce72e5fe75a86c4ef7baf3548e226
Takafumi Shimizu <tkf.smz@gmail.com>
2017-11-26 22:28:46 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/takafumi/shimizu/autoslideshowapp/MainActivity.java
@@ -0,0 +1,162 @@
+package jp.techacademy.takafumi.shimizu.autoslideshowapp;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Handler;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Cursor cursor ;
+    Timer mTimer;
+    Handler mHandler = new Handler();
+    Button button1;
+    Button button2;
+    Button button3;
+
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+
+
+        // Android 6.0以降の場合
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // パーミッションの許可状態を確認する
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // 許可されている
+                getContentsInfo();
+            } else {
+                // 許可されていないので許可ダイアログを表示する
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
+            }
+            // Android 5系以下の場合
+        } else {
+            getContentsInfo();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void getContentsInfo() {
+
+        if (cursor == null) {
+            // 画像の情報を取得する
+            ContentResolver resolver = getContentResolver();
+            cursor = resolver.query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                    null, // 項目(null = 全項目)
+                    null, // フィルタ条件(null = フィルタなし)
+                    null, // フィルタ用パラメータ
+                    null // ソート (null ソートなし)
+            );
+            cursor.moveToFirst();
+        }
+    }
+
+    private void timesCount() {
+        // タイマーの作成
+        mTimer = new Timer();
+        // タイマーの始動
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        if(cursor.isLast()) cursor.moveToFirst();
+                        cursor.moveToNext();
+                        showCursorImage();
+                    }
+                });
+            }
+        }, 0, 2000);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.button1) {
+
+            if(cursor.isLast()) cursor.moveToFirst();
+            cursor.moveToNext();
+            showCursorImage();
+
+        }else if(v.getId() == R.id.button2){
+            if(cursor.isFirst()) cursor.moveToLast();
+            cursor.moveToPrevious();
+            showCursorImage();
+
+        }else if(v.getId() == R.id.button3){
+
+            if(mTimer==null) {
+                timesCount();
+                button1.setEnabled(false);
+                button2.setEnabled(false);
+                button3.setText("停止");
+
+            }else if(mTimer!=null) {
+                mTimer.cancel();
+                button1.setEnabled(true);
+                button2.setEnabled(true);
+                button3.setText("再生");
+                mTimer=null;
+            }
+        }
+    }
+
+    private void showCursorImage(){
+        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+        Long id = cursor.getLong(fieldIndex);
+        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+        ImageView imageVIew = (ImageView) findViewById(R.id.imageView);
+        imageVIew.setImageURI(imageUri);
+    }
+
+
+    @Override protected void onDestroy(){
+        super.onDestroy();
+        cursor.close();
+    }
+}
+
+
+
